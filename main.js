0213d4ef03980bad564cbfebe383ddb5b33a51e9
libertadpozos <libertad.pozos@gmail.com>
2019-04-09 01:51:21 +0200

    Remove not needed files and indent

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
 'use strict';
 
-const btnEl=document.querySelector('.btn');
-const inputEl=document.querySelector('#number');
-const feedbackEl=document.querySelector('.feedback');
-let tryEl=document.querySelector('.try');
-let cont=0;
+const btnEl = document.querySelector('.btn');
+const inputEl = document.querySelector('#number');
+const feedbackEl = document.querySelector('.feedback');
+let tryEl = document.querySelector('.try');
+let cont = 0;
 
 function getRandomNumber(max) {
-    let randomNumber= Math.ceil(Math.random() * max);
-    return randomNumber; 
+  let randomNumber = Math.ceil(Math.random() * max);
+  return randomNumber;
+}
+const randomNumber = (getRandomNumber(100));
+console.log(randomNumber);
+
+function compareNumber() {
+  const numberInput = inputEl.value;
+  const number = parseInt(numberInput);
+  if (number > randomNumber) {
+    feedbackEl.innerHTML = 'demasiado alto ';
   }
-  const randomNumber=(getRandomNumber(100));
-  console.log(randomNumber);
-
-function compareNumber(){
-    const numberInput=inputEl.value;
-    const number=parseInt(numberInput); 
-         if (number>randomNumber){
-             feedbackEl.innerHTML='demasiado alto ';
-         }
-         else if (number<randomNumber){
-             feedbackEl.innerHTML='demasiado bajo';
-         }
-         else if (number===randomNumber){
-            feedbackEl.innerHTML='ERES UNA GANADORA!!!';
-         }
-         cont+=1;
-         tryEl.innerHTML=cont;
- }
-   btnEl.addEventListener('click', compareNumber);
-
-
-   //cómo obtengo el número que da getRandom Number 
-    //   función:
-      //contador que diga cuàntas veces se ha hecho intentos. 
-        //si el número random es mayor al número dado entonces mostrar en la parte de feedback que el número es demasiado alto.
-        // si es menor, mostrar que el número dado es demasiado bajo. 
+  else if (number < randomNumber) {
+    feedbackEl.innerHTML = 'demasiado bajo';
+  }
+  else if (number === randomNumber) {
+    feedbackEl.innerHTML = 'ERES UNA GANADORA!!!';
+  }
+  cont += 1;
+  tryEl.innerHTML = cont;
+}
+btnEl.addEventListener('click', compareNumber);
